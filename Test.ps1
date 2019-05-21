4225ddb3457a0a8f40bcdae6552c851462c09e48
Brad Christie <bch@sitecore.net>
2019-05-21 18:39:27 -0400

    Adding test mechanism to avoid future mishaps

---
+++ Test.ps1
@@ -0,0 +1,45 @@
+[CmdletBinding()]
+Param(
+  [Parameter(Position = 0)]
+  [string[]]$Filter = @("*.json")
+)
+Begin {
+  $eap = $ErrorActionPreference
+
+  $ErrorActionPreference = "Stop"
+
+  Get-Command docker -CommandType Application | Out-Null
+}
+Process {
+  Get-ChildItem "${PSScriptRoot}\src\" -Include $Filter -Recurse | ForEach-Object {
+    $Target = $_.FullName.Replace($PSScriptRoot, ".")
+    $InstallParameters = @("-Path", $Target)
+    $Dockerfile = @"
+# escape=``
+FROM mcr.microsoft.com/windows/servercore/iis:windowsservercore-1803
+SHELL ["powershell.exe", "-Command", "`$ErrorActionPreference='Stop';`$ProgressPreference='SilentlyContinue';"]
+
+# Update NuGet & Install SIF
+RUN Install-PackageProvider -Name 'NuGet' -Force ; ``
+    Install-Module -Name 'PowerShellGet' -Force ; ``
+    Register-PSRepository 'SitecoreGallery' -SourceLocation 'https://sitecore.myget.org/F/sc-powershell/api/v2' -InstallationPolicy Trusted ; ``
+    Install-Module 'SitecoreInstallFramework' -Repository 'SitecoreGallery'
+
+# Copy and run configuration
+RUN New-Item c:\dev\ -ItemType Directory | Out-Null
+WORKDIR /dev
+
+COPY . .
+RUN [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12 ; ``
+    Install-SitecoreConfiguration $($InstallParameters -join " ")
+"@
+
+    $Dockerfile | & docker build --label config="${Target}" -f - .
+    If ($LASTEXITCODE -ne 0) {
+      Write-Error "'${Target}' failed."
+    }
+  }
+}
+End {
+  $ErrorActionPreference = $eap
+}
